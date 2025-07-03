1e60ac5090ad19caad51bc3deb105ce1071bcf75
Kacper Adamski <kacper.adamski@motivait.net>
2025-07-03 11:59:47 +0100

    feat: Learning timeline

--- pages/14a_learn-timeline/script.js
+++ pages/14a_learn-timeline/script.js
@@ -2,16 +2,25 @@ import gsap from "gsap";
 
 const tl = gsap.timeline();
 
-tl.to(".card[data-id='1']", {
- y: 100,
- duration: 2,
- opacity: 1,
- ease: "power1.out",
-});
-
-tl.to(".card[data-id='2']", {
- y: 100,
- duration: 1,
- opacity: 1,
- ease: "power1.out",
-});
+tl
+ .fromTo(
+  ".card[data-id='1']",
+  { y: -100, opacity: 0 },
+  {
+   y: 0,
+   duration: 1,
+   opacity: 1,
+  }
+ )
+ .fromTo(
+  ".card[data-id='2']",
+  {
+   y: -100,
+   opacity: 0,
+  },
+  {
+   y: 0,
+   duration: 1,
+   opacity: 1,
+  }
+ );
